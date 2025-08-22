c8a417e7afe627b3ba6e4ff7ac16e5098f0a44a7
rafasantosit4iot <rafael.santos@it4iot.com>
2025-08-22 10:26:17 -0300

    👌feat: Adaptação do simulador para o Docker

--- simulator/src/main.py
+++ simulator/src/main.py
@@ -1,18 +1,23 @@
 import threading
 import json
+import os
 
 from typing import List
 from flask import Flask
 from flask_mqtt import Mqtt
 
 from features.publishers.temperature_publisher import TemperaturePublisher
+from features.publishers.humidity_publisher import HumidityPublisher
 from shared.mqtt_connection import MqttConnection
 from features.publishers.publisher_abs import Publisher
 from features.subscribers.subscriber import Subscriber
 
+broker = os.getenv("MQTT_BROKER_URL", "127.0.0.1")
+port = int(os.getenv("MQTT_BROKER_PORT", "1883"))
+
 app = Flask(__name__)
-app.config["MQTT_BROKER_URL"] = "127.0.0.1"
-app.config["MQTT_BROKER_PORT"] = 1883
+app.config["MQTT_BROKER_URL"] = broker
+app.config["MQTT_BROKER_PORT"] = port
 app.config["MQTT_USERNAME"] = ""
 app.config["MQTT_PASSWORD"] = ""
 app.config["MQTT_KEEPALIVE"] = 5
@@ -23,17 +28,31 @@ mqtt = Mqtt(app)
 publishers: List[Publisher] = []
 subscriber = Subscriber(mqtt)
 
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 # ----------------------------------------------------------------------------
 # Publishers
 # ----------------------------------------------------------------------------
 def create_publishers(config_file = "publishers.json"):
-    with open(f"./data/{config_file}", "r") as file:
+    with open(f"/simulator/src/data/{config_file}", "r") as file:
         sensors = json.load(file)
 
     for sensor in sensors:
         if sensor["type"] == "temperatura":
             publisher_temp = TemperaturePublisher(
+                mqtt_client=mqtt,
+                topic=sensor["topic"],
+                id=sensor["id"],
+                min_value=sensor.get("min_value", 0),
+                max_value=sensor.get("max_value", 100),
+                interval=sensor.get("interval", 2),
+            )
+            publishers.append(publisher_temp)
+            
+        if sensor["type"] == "umidade":
+            publisher_temp = HumidityPublisher(
                 mqtt_client=mqtt,
                 topic=sensor["topic"],
                 id=sensor["id"],
@@ -73,4 +92,5 @@ def handle_connect(client, userdata, flags, rc):
 
 if __name__ == "__main__":
     mqtt.init_app(app)
-    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
+    print("Serviço iniciado")
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
