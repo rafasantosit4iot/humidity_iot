c62b86ccd4e504ad2c2adb94c9a52f964359c137
rafasantosit4iot <rafael.santos@it4iot.com>
2025-08-22 11:31:34 -0300

    ✨feat: Novo projeto usando o Java 17

---
+++ humidity-iot-api/src/main/java/com/example/humidity_iot_api/HumidityIotApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.humidity_iot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumidityIotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumidityIotApiApplication.class, args);
+	}
+
+}
---
+++ humidity-iot-api/src/test/java/com/example/humidity_iot_api/HumidityIotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humidity_iot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumidityIotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
